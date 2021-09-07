73deb8ebc30167ed4a5098134c6c509d47e18ed5
longrf <longrf@techsure.com.cn>
2021-09-07 15:47:51 +0800

    【苏州银行-资源中心-账号管理优化需求】 https://www.tapd.cn/54247054/prong/stories/view/1154247054001005124

---
+++ src/main/java/codedriver/module/tagent/api/runner/RunnerGroupSearchApi.java
@@ -0,0 +1,54 @@
+package codedriver.module.tagent.api.runner;
+
+import codedriver.framework.auth.core.AuthAction;
+import codedriver.framework.autoexec.dto.RunnerGroupVo;
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.*;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
+import codedriver.framework.tagent.auth.label.TAGENT_BASE;
+import codedriver.framework.tagent.dao.mapper.TagentMapper;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = TAGENT_BASE.class)
+@OperationType(type = OperationTypeEnum.SEARCH)
+public class RunnerGroupSearchApi extends PrivateApiComponentBase {
+
+    @Resource
+    TagentMapper tagentMapper;
+
+    @Override
+    public String getName() {
+        return "Tagent代理组查询接口";
+    }
+
+    @Override
+    public String getToken() {
+        return "tagent/runnergroup/search";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Description(desc = "获取tagent代理组信息")
+    @Input({
+            @Param(name = "keyword",type = ApiParamType.STRING,desc = "关键词")
+    })
+    @Output({
+            @Param(name = "tagentRunnerGroupInformation",explode = RunnerGroupVo[].class,desc = "所有tagent代理组")
+    })
+    @Override
+    public Object myDoService(JSONObject paramObj) throws Exception {
+        RunnerGroupVo groupVo =JSONObject.toJavaObject(paramObj,RunnerGroupVo.class);
+//        int rowNum = tagentMapper.searchRunnerGroupCount();
+        return tagentMapper.searchRunnerGroupInformation(groupVo);
+    }
+
+
+}
