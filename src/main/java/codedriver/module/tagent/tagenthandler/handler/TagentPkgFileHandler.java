274c33b98c07c5612f545cdfe1480187bef6ba71
陈其炜 <chenqw@techsure.com.cn>
2021-11-02 11:58:33 +0800

    调整附件处理接口
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/tagent/tagenthandler/handler/TagentPkgFileHandler.java
+++ src/main/java/codedriver/module/tagent/tagenthandler/handler/TagentPkgFileHandler.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.tagent.tagenthandler.handler;
 
 import codedriver.framework.asynchronization.threadlocal.UserContext;
@@ -18,8 +23,14 @@ public class TagentPkgFileHandler extends FileTypeHandlerBase {
     private TagentMapper tagentMapper;
 
     @Override
-    protected boolean myDeleteFile(Long fileId) {
-        return false;
+    protected boolean myDeleteFile(FileVo fileVo, JSONObject paramObj) {
+        Long pkgVersionId = paramObj.getLong("pkgVersionId");
+        Long fileId = fileVo.getId();
+        if (tagentMapper.getTagentVersionById(pkgVersionId) == null) {
+            throw new TagentPkgVersionIdNotFoundException(pkgVersionId);
+        }
+        tagentMapper.deleteTagentVersionById(pkgVersionId);
+        return tagentMapper.getTagentPkgFileIdUsedCount(fileId) <= 1;
     }
 
     @Override
@@ -40,27 +51,5 @@ public class TagentPkgFileHandler extends FileTypeHandlerBase {
         return "tagent安装包文件";
     }
 
-    @Override
-    public void afterUpload(FileVo fileVo, JSONObject jsonObj) {
 
-    }
-
-    @Override
-    public boolean beforeUpload(JSONObject jsonObj) {
-        return true;
-    }
-
-    @Override
-    public boolean beforeDelete(FileVo fileVo, JSONObject jsonObj) {
-        Long pkgVersionId = jsonObj.getLong("pkgVersionId");
-        Long fileId = fileVo.getId();
-        if (tagentMapper.getTagentVersionById(pkgVersionId) == null) {
-            throw new TagentPkgVersionIdNotFoundException(pkgVersionId);
-        }
-        tagentMapper.deleteTagentVersionById(pkgVersionId);
-        if (tagentMapper.getTagentPkgFileIdUsedCount(fileId) > 1) {
-            return false;
-        }
-        return true;
-    }
 }
