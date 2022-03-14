8c4ddd50e7b8f7133cbc2be84832f3729faa2643
longrf <techsure901>
2022-03-14 15:19:33 +0800

    --story=1006005 --user=龙儒锋 增加批量查询依赖关系的入口 https://www.tapd.cn/54247054/s/1213968

---
+++ src/main/java/codedriver/module/tagent/dependency/TagentAccountDependencyHandler.java
@@ -0,0 +1,68 @@
+package codedriver.module.tagent.dependency;
+
+import codedriver.framework.dependency.core.CustomTableDependencyHandlerBase;
+import codedriver.framework.dependency.core.IFromType;
+import codedriver.framework.dependency.dto.DependencyInfoVo;
+import codedriver.framework.tagent.enums.TagentFromType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author longrf
+ * @date 2022/3/10 11:51 上午
+ */
+@Service
+public class TagentAccountDependencyHandler extends CustomTableDependencyHandlerBase {
+
+    /**
+     * 表名
+     *
+     * @return
+     */
+    @Override
+    protected String getTableName() {
+        return "tagent";
+    }
+
+    /**
+     * 被引用者（上游）字段
+     *
+     * @return
+     */
+    @Override
+    protected String getFromField() {
+        return "account_id";
+    }
+
+    /**
+     * 引用者（下游）字段
+     *
+     * @return
+     */
+    @Override
+    protected String getToField() {
+        return "id";
+    }
+
+    @Override
+    protected List<String> getToFieldList() {
+        return null;
+    }
+
+    @Override
+    protected DependencyInfoVo parse(Object dependencyObj) {
+        return null;
+    }
+
+    /**
+     * 被引用者（上游）类型
+     *
+     * @return
+     */
+    @Override
+    public IFromType getFromType() {
+        return TagentFromType.TAGENT_ACCOUNT;
+    }
+}
+
