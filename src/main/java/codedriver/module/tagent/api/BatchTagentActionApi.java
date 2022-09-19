3a646108ad5a630b78f8a1108118566ac5d1cec2
longrf <a5739119>
2022-09-19 17:04:59 +0800

    --story=1006906 --user=龙儒锋 tagent 批量重启 https://www.tapd.cn/54247054/s/1252596

--- src/main/java/codedriver/module/tagent/api/BatchTagentActionApi.java
+++ src/main/java/codedriver/module/tagent/api/BatchTagentActionApi.java
@@ -5,9 +5,7 @@
 package codedriver.module.tagent.api;
 
 import codedriver.framework.auth.core.AuthAction;
-import codedriver.framework.cmdb.dto.resourcecenter.IpVo;
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.dto.runner.NetworkVo;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
@@ -15,16 +13,12 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.tagent.auth.label.TAGENT_BASE;
-import codedriver.framework.tagent.dto.TagentVo;
-import codedriver.framework.tagent.enums.TagentAction;
+import codedriver.framework.tagent.dto.TagentSearchVo;
 import codedriver.framework.tagent.service.TagentService;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author longrf
@@ -34,14 +28,14 @@ import java.util.List;
 @Service
 @AuthAction(action = TAGENT_BASE.class)
 @OperationType(type = OperationTypeEnum.OPERATE)
-public class BatchResetPasswordApi extends PrivateApiComponentBase {
+public class BatchTagentActionApi extends PrivateApiComponentBase {
 
     @Resource
     TagentService tagentService;
 
     @Override
     public String getName() {
-        return "批量重置tagent密码";
+        return "批量操作tagent";
     }
 
     @Override
@@ -50,38 +44,20 @@ public class BatchResetPasswordApi extends PrivateApiComponentBase {
     }
 
     @Input({
+            @Param(name = "action", type = ApiParamType.STRING, desc = "tagent动作（reload(重启)、resetcred(重置密码)）"),
             @Param(name = "ipPortList", type = ApiParamType.JSONARRAY, desc = "ip,port列表"),
             @Param(name = "networkVoList", type = ApiParamType.JSONARRAY, desc = "网段"),
             @Param(name = "runnerGroupIdList", type = ApiParamType.JSONARRAY, desc = "代理组id")
     })
-    @Description(desc = "批量重置tagent密码")
+    @Description(desc = "批量操作tagent")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        JSONArray ipPortArray = paramObj.getJSONArray("ipPortList");
-        JSONArray networkVoArray = paramObj.getJSONArray("networkVoList");
-        JSONArray runnerGroupIdArray = paramObj.getJSONArray("runnerGroupIdList");
-
-        List<NetworkVo> networkVoList = null;
-        List<IpVo> ipPortList = null;
-        List<Long> runnerGroupIdList = null;
-        if (CollectionUtils.isNotEmpty(networkVoArray)) {
-            networkVoList = networkVoArray.toJavaList(NetworkVo.class);
-        }
-        if (CollectionUtils.isNotEmpty(ipPortArray)) {
-            ipPortList = ipPortArray.toJavaList(IpVo.class);
-        }
-        if (CollectionUtils.isNotEmpty(runnerGroupIdArray)) {
-            runnerGroupIdList = runnerGroupIdArray.toJavaList(Long.class);
-        }
-        List<TagentVo> tagentList = tagentService.getTagentList(ipPortList, networkVoList, runnerGroupIdList);
-        if (CollectionUtils.isEmpty(tagentList)) {
-            return null;
-        }
-        return tagentService.batchExecTagentChannelAction(TagentAction.RESET_CREDENTIAL, tagentList);
+        TagentSearchVo tagentSearchVo = paramObj.toJavaObject(TagentSearchVo.class);
+        return tagentService.batchExecTagentChannelAction(paramObj.getString("action"), tagentSearchVo.getIpPortList(), tagentSearchVo.getNetworkVoList(), tagentSearchVo.getRunnerGroupIdList());
     }
 
     @Override
     public String getToken() {
-        return "tagent/exec/password/batch/reset";
+        return "tagent/exec/batch/action";
     }
 }
