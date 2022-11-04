a1b9ac7484955f17087da382d76bb2cd44c60bcd
longrf <a5739119>
2022-11-04 15:59:39 +0800

    --story=1007075 --user=龙儒锋 tagent-批量修改配置文件 https://www.tapd.cn/54247054/s/1260049

--- src/main/java/codedriver/module/tagent/api/BatchTagentActionApi.java
+++ src/main/java/codedriver/module/tagent/api/BatchTagentActionApi.java
@@ -6,6 +6,7 @@ package codedriver.module.tagent.api;
 
 import codedriver.framework.auth.core.AuthAction;
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.exception.type.ParamIrregularException;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
@@ -13,7 +14,9 @@ import codedriver.framework.restful.annotation.Param;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.framework.tagent.auth.label.TAGENT_BASE;
+import codedriver.framework.tagent.dto.TagentMessageVo;
 import codedriver.framework.tagent.dto.TagentSearchVo;
+import codedriver.framework.tagent.enums.TagentAction;
 import codedriver.framework.tagent.service.TagentService;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Service;
@@ -44,7 +47,8 @@ public class BatchTagentActionApi extends PrivateApiComponentBase {
     }
 
     @Input({
-            @Param(name = "action", type = ApiParamType.STRING, desc = "tagent动作（reload(重启)、resetcred(重置密码)）"),
+            @Param(name = "action", type = ApiParamType.STRING, isRequired = true, desc = "tagent动作（reload(重启)、resetcred(重置密码)、batchSaveConfig(批量修改配置文件)）"),
+            @Param(name = "configKeyValueJson", type = ApiParamType.STRING, desc = "配置key、value的Json，此参数在tction为 batchSaveConfig时才会用到"),
             @Param(name = "ipPortList", type = ApiParamType.JSONARRAY, desc = "ip,port列表"),
             @Param(name = "networkVoList", type = ApiParamType.JSONARRAY, desc = "网段列表"),
             @Param(name = "runnerGroupIdList", type = ApiParamType.JSONARRAY, desc = "执行器组id列表")
@@ -52,7 +56,14 @@ public class BatchTagentActionApi extends PrivateApiComponentBase {
     @Description(desc = "批量操作tagent")
     @Override
     public Object myDoService(JSONObject paramObj) throws Exception {
-        return tagentService.batchExecTagentChannelAction(paramObj.getString("action"), paramObj.toJavaObject(TagentSearchVo.class));
+        TagentMessageVo tagentMessageVo = new TagentMessageVo();
+        if (TagentAction.BATCH_SAVE_CONFIG.getValue().equals(paramObj.getString("action"))) {
+            if (paramObj.getJSONObject("configKeyValueJson") == null) {
+                throw new ParamIrregularException("configKeyValueJson");
+            }
+            tagentMessageVo.setData(paramObj.getJSONObject("configKeyValueJson").toString());
+        }
+        return tagentService.batchExecTagentChannelAction(paramObj.getString("action"), paramObj.toJavaObject(TagentSearchVo.class), tagentMessageVo);
     }
 
     @Override
